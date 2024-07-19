0c600583ea667440892c832c5239451141848799
Mustafa khan <122636626+HareemRehan13@users.noreply.github.com>
2024-07-19 17:08:39 +0500

    first class work

---
+++ index.ts
@@ -0,0 +1,19 @@
+let fruits :string[] = ["orang","mango","apple"];
+
+fruits.forEach(fruits=>{
+    console.log(fruits);
+})
+
+let person :{names:string,age:number} []=[{names:"hadi",age:20000},{names:"haziq",age:18}];
+    console.log(person);
+
+
+let num1 :number =20;
+let num2 :number =25;
+
+function add(num1:number,num2:number){
+    return num1 + num2 ;
+}
+console.log(add(num1,num2));
+
+// let age=18;
